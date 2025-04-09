5fde9912894d37131fd12cd0d5fe103b70135763
aceC0der <paulmridul.mp@gmail.com>
2025-04-09 10:06:53 +0600

    connected database

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
 const app = express();
+connectDb();
 
 const port = process.env.PORT || 5000;
 
